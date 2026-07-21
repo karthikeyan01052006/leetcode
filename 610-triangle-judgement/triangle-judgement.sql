d6172508696657f373b5edb7bd65067a5f789a9b
karthikeyan01052006 <tmdkarthikeyan@gmail.com>
2026-07-21 15:15:00 +0530

    Time: 336 ms (18.56%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -3,9 +3,7 @@ SELECT x,
        y,
        z,
        CASE
-           WHEN x + y > z
-            AND x + z > y
-            AND y + z > x
+           WHEN x + y > z AND x + z > y  AND y + z > x
            THEN 'Yes'
            ELSE 'No'
        END AS triangle
